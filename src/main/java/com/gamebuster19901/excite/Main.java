43d5b3713066d985455e864f84fe8bece9891e36
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2020-04-29 14:50:03 -0400

    Fix indentation

--- src/main/java/com/gamebuster19901/excite/Main.java
+++ src/main/java/com/gamebuster19901/excite/Main.java
@@ -16,7 +16,7 @@ public class Main {
 	public static Wiimmfi wiimmfi;
 	public static DiscordBot discordBot;
 	
-public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException {
 	
 		if(args.length % 2 != 0) {
 			throw new IllegalArgumentException("Must be started with an even number of arguments!");
@@ -31,61 +31,61 @@ public static void main(String[] args) throws InterruptedException {
 			LOGGER.log(Level.SEVERE, e, () -> e.getMessage());
 		}
 	
-	Throwable prevError = null;
-	discordBot.updatePresence();
-	while(true) {
-		wiimmfi.update();
-		Throwable error = wiimmfi.getError();
-		if(error == null) {
-			if(prevError != null) {
-				LOGGER.log(Level.SEVERE, "Error resolved.");
+		Throwable prevError = null;
+		discordBot.updatePresence();
+		while(true) {
+			wiimmfi.update();
+			Throwable error = wiimmfi.getError();
+			if(error == null) {
+				if(prevError != null) {
+					LOGGER.log(Level.SEVERE, "Error resolved.");
+				}
+				
+				Player[] onlinePlayers = wiimmfi.getOnlinePlayers();
+				Player.updatePlayerListFile();
+				
+				LOGGER.info("Players online: " + onlinePlayers.length);
+				int waitTime = 60000;
+				if(onlinePlayers.length > 1) {
+					waitTime = waitTime / onlinePlayers.length;
+					if(waitTime < 4000) {
+						waitTime = 4000;
+					}
+				}
+				Thread.sleep(waitTime);
 			}
-			
-			Player[] onlinePlayers = wiimmfi.getOnlinePlayers();
-			Player.updatePlayerListFile();
-			
-			LOGGER.info("Players online: " + onlinePlayers.length);
-			int waitTime = 60000;
-			if(onlinePlayers.length > 1) {
-				waitTime = waitTime / onlinePlayers.length;
-				if(waitTime < 4000) {
-					waitTime = 4000;
+			else {
+				Thread.sleep(5000);
+				if(prevError == null || !prevError.getClass().equals(error.getClass())) {
+					System.out.println("Error!");
+					LOGGER.log(Level.SEVERE, error, () -> error.getMessage());
 				}
 			}
-			Thread.sleep(waitTime);
+				discordBot.updatePresence();
+			prevError = error;
 		}
-		else {
-			Thread.sleep(5000);
-			if(prevError == null || !prevError.getClass().equals(error.getClass())) {
-				System.out.println("Error!");
-				LOGGER.log(Level.SEVERE, error, () -> error.getMessage());
+	}
+
+	private static Wiimmfi startWiimmfi(String[] args) {
+		for(int i = 0; i < args.length; i++) {
+				if(args[i].equalsIgnoreCase("-url")) {
+					return new Wiimmfi(args[++i]);
+				}
+		}
+		return new Wiimmfi();
+	}
+	
+	private static DiscordBot startDiscordBot(String[] args, Wiimmfi wiimmfi) throws LoginException, IOException {
+			String botOwner = null;
+			File keyFile = new File("./discord.secret");
+		for(int i = 0; i < args.length; i++) {
+			if(args[i].equalsIgnoreCase("-owner")) {
+				botOwner = args[++i];
+			}
+			if(args[i].equalsIgnoreCase("-keyFile")) {
+				keyFile = new File(args[++i]);
 			}
 		}
-			discordBot.updatePresence();
-		prevError = error;
+		return new DiscordBot(wiimmfi, botOwner, keyFile);
 	}
-}
-
-private static Wiimmfi startWiimmfi(String[] args) {
-	for(int i = 0; i < args.length; i++) {
-			if(args[i].equalsIgnoreCase("-url")) {
-				return new Wiimmfi(args[++i]);
-			}
-	}
-	return new Wiimmfi();
-}
-
-private static DiscordBot startDiscordBot(String[] args, Wiimmfi wiimmfi) throws LoginException, IOException {
-		String botOwner = null;
-		File keyFile = new File("./discord.secret");
-	for(int i = 0; i < args.length; i++) {
-		if(args[i].equalsIgnoreCase("-owner")) {
-			botOwner = args[++i];
-		}
-		if(args[i].equalsIgnoreCase("-keyFile")) {
-			keyFile = new File(args[++i]);
-		}
-	}
-	return new DiscordBot(wiimmfi, botOwner, keyFile);
-}
 }
