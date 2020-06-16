50b1424d0b9c588a6f52312dbfcf31c4be049b76
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2020-06-16 06:30:34 -0400

    Fix #27

--- src/main/java/com/gamebuster19901/excite/bot/command/OnlineCommand.java
+++ src/main/java/com/gamebuster19901/excite/bot/command/OnlineCommand.java
@@ -5,17 +5,18 @@ import com.gamebuster19901.excite.Wiimmfi;
 import com.gamebuster19901.excite.bot.WiimmfiCommand;
 import com.gamebuster19901.excite.util.StacktraceUtil;
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 
 @SuppressWarnings("rawtypes") 
 public class OnlineCommand extends WiimmfiCommand{
 
 	public static void register(CommandDispatcher<MessageContext> dispatcher) {
-		LiteralCommandNode<MessageContext> node = dispatcher.register(Commands.literal("!online").executes((command) -> {
+		LiteralArgumentBuilder<MessageContext> builder = Commands.literal("!online").executes((command) -> {
 			return sendResponse(command.getSource());
-		}));
+		});
 		
-		dispatcher.register(Commands.literal("!o").redirect(node));
+		dispatcher.register(Commands.literal("!o").executes(builder.getCommand()));
 	}
 	
 	public static int sendResponse(MessageContext context) {
