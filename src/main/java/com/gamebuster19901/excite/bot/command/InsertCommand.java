02160ac1559795fa54ea9571d985a868021e2bb5
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2021-01-19 03:07:05 -0500

    Change insertion message

--- src/main/java/com/gamebuster19901/excite/bot/command/InsertCommand.java
+++ src/main/java/com/gamebuster19901/excite/bot/command/InsertCommand.java
@@ -29,7 +29,7 @@ public class InsertCommand {
 						name = name.replace("\"", "");
 						Player.addPlayer(context, false, pid, fc, name);
 						Player player = Player.getPlayerByID(context, pid);
-						context.sendMessage(player.toString() + " inserted");
+						context.sendMessage("Inserted" + player.toFullString());
 						return 1;
 					}
 					else {
