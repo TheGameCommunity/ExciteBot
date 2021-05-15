b744294f10325ba5be5f2061cdf186b83ba04db3
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2021-05-15 03:27:21 -0400

    Add Challenge class

---
+++ src/main/java/com/gamebuster19901/excite/game/challenge/Challenge.java
@@ -0,0 +1,18 @@
+package com.gamebuster19901.excite.game.challenge;
+
+import com.gamebuster19901.excite.game.Bot;
+import com.gamebuster19901.excite.game.Course;
+import com.gamebuster19901.excite.util.file.File;
+
+public class Challenge {
+
+	private final Course course;
+	private final short reward;
+	private final Bot bot;
+	private byte[] challengeData;
+	
+	public Challenge(File file) {
+		
+	}
+	
+}
---
+++ src/main/java/com/gamebuster19901/excite/game/challenge/ChallengeFrame.java
@@ -0,0 +1,24 @@
+package com.gamebuster19901.excite.game.challenge;
+
+import java.io.InputStream;
+
+public class ChallengeFrame {
+
+	byte unknown1;
+	byte unknown2;
+	byte unknown3;
+	byte unknown4;
+	byte unknown5;
+	byte unknown6;
+	byte starStatus; //visibility of the star ui at the top of the screen? Values: F, B, 7, 4
+	byte currentStars;
+	byte unknown9;
+	byte unknown10;
+	byte unknown11;
+	byte unknown12;
+	
+	public void consume(InputStream inputStream) {
+		
+	}
+	
+}
