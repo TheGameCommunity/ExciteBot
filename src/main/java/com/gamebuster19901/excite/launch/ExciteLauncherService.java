fa72d54a375d43b9d64b7b200866e56a3d48c7a0
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2021-07-13 19:28:55 -0400

    Hack TransformingClassloader to allow transformation of .javax classes
    ah yes, hack the thing you use to hack the thing you use to communicate
    with the thing which hacks the game

--- src/main/java/com/gamebuster19901/excite/launch/ExciteLauncherService.java
+++ src/main/java/com/gamebuster19901/excite/launch/ExciteLauncherService.java
@@ -2,11 +2,17 @@ package com.gamebuster19901.excite.launch;
 
 import java.io.File;
 import java.io.IOError;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.Callable;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 import cpw.mods.gross.Java9ClassLoaderUtil;
+import cpw.mods.modlauncher.TransformingClassLoader;
 import cpw.mods.modlauncher.api.ILaunchHandlerService;
 import cpw.mods.modlauncher.api.ITransformingClassLoader;
 import cpw.mods.modlauncher.api.ITransformingClassLoaderBuilder;
@@ -24,8 +30,10 @@ public class ExciteLauncherService implements ILaunchHandlerService {
 
 	@Override
 	public void configureTransformationClassLoader(ITransformingClassLoaderBuilder builder) {
+		grossClassTransformerHackery(); //gross classtransformer hack, THIS COULD BREAK IN FUTURE MODLAUNCHER RELEASES
 		for (final URL url : Java9ClassLoaderUtil.getSystemClassPathURLs()) {
 			try {
+				System.out.println(url);
 				builder.addTransformationPath(new File(url.toURI()).toPath());
 			} catch (Throwable t) {
 				throw new IOError(new Error("Could not start ExciteLauncher service!", t));
@@ -41,6 +49,27 @@ public class ExciteLauncherService implements ILaunchHandlerService {
 			mainMethod.invoke(null, new Object[] {arguments});
 			return null;
 		};
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void grossClassTransformerHackery() {
+		try {
+			Field skipPrefixField = TransformingClassLoader.class.getDeclaredField("SKIP_PACKAGE_PREFIXES");
+			skipPrefixField.setAccessible(true);
+			List<String> disallowedPrefixList = (List<String>) skipPrefixField.get(null);
+			Field listArray = disallowedPrefixList.getClass().getDeclaredField("a");
+			listArray.setAccessible(true);
+			String[] disallowedPrefixes = (String[]) listArray.get(disallowedPrefixList);
+			disallowedPrefixes = ArrayUtils.remove(disallowedPrefixes, 1);
+			listArray.set(disallowedPrefixList, disallowedPrefixes);
+			System.out.println(Arrays.toString(disallowedPrefixList.toArray()));
+			if(disallowedPrefixList.contains(".javax")) {
+				throw new LinkageError("Modlauncher probably changed SKIP_PACKAGE_PREFIXES", new IllegalStateException("Removal of \".javax\" from disallowed packages failed."));
+			}
+		}
+		catch(NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+			throw new AssertionError(e);
+		}
 	}
 	
 }
