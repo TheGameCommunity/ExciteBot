c3b482d1cec0a1749f796c6fea4557e0a3ea43c3
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2021-07-17 06:14:53 -0400

    Update structure.sql

--- src/main/resources/structure.sql
+++ src/main/resources/structure.sql
@@ -1,8 +1,8 @@
 -- MySQL dump 10.13  Distrib 8.0.22, for Linux (x86_64)
 --
--- Host: sql.gamebuster19901.com    Database: excitebot
+-- Host: localhost    Database: excitetest
 -- ------------------------------------------------------
--- Server version	8.0.22
+-- Server version	8.0.25-0ubuntu0.21.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -48,6 +48,20 @@ CREATE TABLE `audit_bans` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `audit_bot_msg_del`
+--
+
+DROP TABLE IF EXISTS `audit_bot_msg_del`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `audit_bot_msg_del` (
+  `auditID` bigint unsigned NOT NULL,
+  `discordID` bigint unsigned NOT NULL,
+  PRIMARY KEY (`auditID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `audit_commands`
 --
 
@@ -60,6 +74,7 @@ CREATE TABLE `audit_commands` (
   `serverID` bigint unsigned NOT NULL,
   `channelName` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `channelID` bigint unsigned NOT NULL,
+  `messageID` bigint unsigned NOT NULL DEFAULT '0',
   `isGuildMessage` bit(1) NOT NULL,
   `isPrivateMessage` bit(1) NOT NULL,
   `isConsoleMessage` bit(1) NOT NULL,
@@ -79,8 +94,8 @@ DROP TABLE IF EXISTS `audit_name_changes`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `audit_name_changes` (
   `auditID` bigint unsigned NOT NULL,
-  `oldName` varchar(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `newName` varchar(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `oldName` varchar(17) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `newName` varchar(17) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `playerID` int NOT NULL,
   `friendCode` varchar(14) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   PRIMARY KEY (`auditID`),
@@ -112,6 +127,36 @@ DROP TABLE IF EXISTS `audit_profile_discoveries`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `audit_profile_discoveries` (
+  `auditID` bigint unsigned NOT NULL,
+  `playerID` bigint NOT NULL,
+  PRIMARY KEY (`auditID`),
+  UNIQUE KEY `auditID_UNIQUE` (`auditID`),
+  UNIQUE KEY `playerID_UNIQUE` (`playerID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `audit_profile_logins`
+--
+
+DROP TABLE IF EXISTS `audit_profile_logins`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `audit_profile_logins` (
+  `auditID` bigint unsigned NOT NULL,
+  PRIMARY KEY (`auditID`),
+  UNIQUE KEY `auditID_UNIQUE` (`auditID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `audit_profile_logouts`
+--
+
+DROP TABLE IF EXISTS `audit_profile_logouts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `audit_profile_logouts` (
   `auditID` bigint unsigned NOT NULL,
   PRIMARY KEY (`auditID`),
   UNIQUE KEY `auditID_UNIQUE` (`auditID`)
@@ -150,7 +195,7 @@ CREATE TABLE `audits` (
   `issued` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   PRIMARY KEY (`auditID`),
   UNIQUE KEY `auditID_UNIQUE` (`auditID`)
-) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=3585 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -181,11 +226,33 @@ CREATE TABLE `discord_users` (
   `discord_name` char(37) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `threshold` int NOT NULL DEFAULT '-1',
   `frequency` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT 'PT30M',
-  `lastNotification` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `lastNotification` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `dippedBelowThreshold` tinyint(1) NOT NULL DEFAULT '0',
   `notifyContinuously` tinyint(1) NOT NULL DEFAULT '0',
+  `detailedPM` tinyint(1) NOT NULL DEFAULT '0',
+  `stars` bigint NOT NULL DEFAULT '0',
   PRIMARY KEY (`discordID`),
   UNIQUE KEY `discord_id_UNIQUE` (`discordID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `mail`
+--
+
+DROP TABLE IF EXISTS `mail`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `mail` (
+  `auditID` bigint unsigned NOT NULL,
+  `sender` varchar(320) COLLATE utf8mb4_bin NOT NULL,
+  `receiver` varchar(320) COLLATE utf8mb4_bin NOT NULL,
+  `incoming` tinyint(1) NOT NULL,
+  `mailType` varchar(45) COLLATE utf8mb4_bin NOT NULL,
+  `file` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`auditID`),
+  UNIQUE KEY `auditID_UNIQUE` (`auditID`),
+  UNIQUE KEY `file_UNIQUE` (`file`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -213,11 +280,11 @@ DROP TABLE IF EXISTS `players`;
 CREATE TABLE `players` (
   `playerID` int NOT NULL,
   `friendCode` char(14) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `name` varchar(8) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `redacted` tinyint(1) NOT NULL DEFAULT '0',
-  `discordID` bigint unsigned DEFAULT NULL,
-  `lastOnline` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '2020-11-01T02:00:00+5000',
-  `secondsPlayed` bigint NOT NULL DEFAULT '0',
+  `discordID` bigint unsigned DEFAULT '0',
+  `lastOnline` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '2020-11-01T07:00:00Z',
+  `secondsPlayed` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT 'PT0S',
   PRIMARY KEY (`playerID`),
   UNIQUE KEY `player_id_UNIQUE` (`playerID`),
   UNIQUE KEY `friendCode_UNIQUE` (`friendCode`)
@@ -225,25 +292,177 @@ CREATE TABLE `players` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `players_backup`
+-- Table structure for table `wiis`
 --
 
-DROP TABLE IF EXISTS `players_backup`;
+DROP TABLE IF EXISTS `wiis`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `players_backup` (
-  `playerID` int NOT NULL,
-  `friendCode` char(14) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `name` varchar(8) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `redacted` tinyint(1) NOT NULL DEFAULT '0',
-  `discordID` bigint unsigned DEFAULT NULL,
-  `lastOnline` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '2020-11-01T02:00:00+5000',
-  `secondsPlayed` bigint NOT NULL DEFAULT '0',
-  PRIMARY KEY (`playerID`),
-  UNIQUE KEY `player_id_UNIQUE` (`playerID`),
-  UNIQUE KEY `friendCode_UNIQUE` (`friendCode`)
+CREATE TABLE `wiis` (
+  `wiiID` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `discordID` bigint unsigned NOT NULL DEFAULT '0',
+  `registrationCode` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`wiiID`),
+  UNIQUE KEY `wiis_UNIQUE` (`wiiID`),
+  UNIQUE KEY `registrationCode_UNIQUE` (`registrationCode`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping routines for database 'excitetest'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `audits` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`Admin`@`%` PROCEDURE `audits`()
+BEGIN
+select * from audits;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `bans` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`gamebuster`@`%` PROCEDURE `bans`()
+BEGIN
+SELECT * FROM audits INNER JOIN audit_bans WHERE audit_bans.auditID = audits.auditID;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `bansAndPardons` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`gamebuster`@`%` PROCEDURE `bansAndPardons`()
+BEGIN
+SELECT * FROM audits WHERE audits.type = "BAN" OR audits.type = "PARDON";
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `commands` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`gamebuster`@`%` PROCEDURE `commands`()
+BEGIN
+SELECT * FROM audits INNER JOIN audit_commands WHERE audit_commands.auditID = audits.auditID;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `name_changes` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`gamebuster`@`localhost` PROCEDURE `name_changes`()
+BEGIN
+SELECT * FROM audits INNER JOIN audit_name_changes WHERE audit_name_changes.auditID = audits.auditID;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `pardons` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`gamebuster`@`%` PROCEDURE `pardons`()
+BEGIN
+SELECT * from audits INNER JOIN audit_pardons WHERE audit_pardons.auditID = audits.auditID;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `profile_discoveries` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`gamebuster`@`%` PROCEDURE `profile_discoveries`()
+BEGIN
+SELECT * FROM audits INNER JOIN audit_profile_discoveries WHERE audit_profile_discoveries.auditID = audits.auditID;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `rank_changes` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`gamebuster`@`%` PROCEDURE `rank_changes`()
+BEGIN
+SELECT * FROM audits INNER JOIN audit_rank_changes WHERE audit_rank_changes.auditID = audits.auditID;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -254,4 +473,4 @@ CREATE TABLE `players_backup` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-11-18  5:34:41
+-- Dump completed on 2021-07-17  6:08:01
